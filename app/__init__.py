a3b5083214037fe23a13c0adae53fd3870ddca67
YueYingLee <yueyinglee@gmail.com>
2024-09-28 18:43:55 -0700

    initial set up

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+myapp_obj = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+myapp_obj.config.from_mapping(
+    SECRET_KEY = 'you-will-never-guess',
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db'),
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+)
+
+db = SQLAlchemy(myapp_obj)
+login = LoginManager(myapp_obj)
+login.login_view = 'login'
+
+from app import routes
+#from app.models import User, Role, Book, Profile 
---
+++ app/routes.py
@@ -0,0 +1,2 @@
+//temp routes.py 
+
